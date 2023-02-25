3cadc86d5c7c5a580792440c6ba52f744ded8c83
0xk35b4r <kesinbarandir@gmail.com>
2023-02-25 04:10:06 +0300

    KDV Hesaplayan Program github a eklendi

---
+++ DikUcgendeHipotenusBulanProgram.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class DikUcgendeHipotenusBulanProgram {
+    public static void main(String[] args) {
+        // Değişkenler
+        int a, b;
+        double c;
+
+        // Scanner Import
+        // Kullanıcıdan Veri Alma
+        Scanner input = new Scanner(System.in);
+        System.out.print("a kenar uzunluğunu giriniz : ");
+        a = input.nextInt();
+        System.out.print("b kenar uzunluğunu giriniz : ");
+        b = input.nextInt();
+
+        c = Math.sqrt((a*a) + (b*b));
+        System.out.println(c);
+
+
+        
+    }
+}
